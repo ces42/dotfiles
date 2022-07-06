3cb6a38f92d1d27ffb2183b7791c69f0cff56d14
Carlos Esparza <ca2010es@gmail.com>
2022-07-07 00:40:51 +0200

    scripts in ~/bin/

---
+++ bin/_fix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://askubuntu.com/questions/1262135/gnome-shell-font-scaling-resetting
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.1
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.09
